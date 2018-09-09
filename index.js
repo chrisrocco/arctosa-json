2316aa6818a8e23ea21d73bcefd97c74c9f8b2b7
Chris Rocco <chris.rocco7@gmail.com>
2018-09-09 10:50:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const dotenv = require('dotenv')
+const PubSub = require(`@google-cloud/pubsub`)
+
+dotenv.config()
+const outputDir = 'results'
+
+const pubsub = new PubSub()
+const subscription = pubsub.subscription('rdbs-stream')
+
+subscription.on('message', event => {
+    const dataObject = JSON.parse(event.data.toString())
+    const contents = JSON.stringify(dataObject, null, 2)
+    fs.writeFileSync('results/' + event.id + '.json', contents)
+    event.ack()
+    console.log('wrote file ', event.id + '.json')
+})
+
+console.log('running json streaming worker..')
+console.log('press ctrl + c to stop')
